45a2c975580b716398c270fbeec69879dfc8ac56
Dirk Dobson <dirkvancouver@live.com>
2018-12-10 05:47:38 -0700

    intal commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace csPaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @":\Projects/CSharpFundamentals\HellowWorld\HellowWorld.sln";
+
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File Name: " + Path.GetFileName(path));
+            Console.WriteLine("File Name without Extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Get Direcotry Name " + Path.GetDirectoryName(path));
+        }
+    }
+}
